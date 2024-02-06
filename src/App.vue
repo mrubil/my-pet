0265a7f58a5689ddeb5fc48fb4cfa673f401b4ff
Martina Rubil <92204071+mrubil@users.noreply.github.com>
2024-02-06 17:47:07 +0100

    update

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <template>
   <div id="app">
+    <h1>Dobrodošli na <br /></h1>
+    <div>
+      <img src="src\assets\White_paw_print.png" />
+    </div>
+    <br />
+    <h1>MyPet</h1>
+
     <nav>
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/SignUp">Prijava</router-link>
     </nav>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
@@ -14,19 +21,11 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #000000;
+  background: #ff344c;
 }
 
 nav {
   padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
