ef70f592374ddcfe987d6be4b9f0fca8021f1dfc
Martina Rubil <92204071+mrubil@users.noreply.github.com>
2024-02-09 17:37:23 +0100

    Update Login.vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,73 @@
 <template>
-  <div class="Login">
-    <h1>This is a Login page</h1>
-  </div>
+  <body>
+    <div class="Zaglavlje">
+      <h2 style="font-weight: 600">Prijava</h2>
+    </div>
+    <div>
+      <img :src="require('@/assets/White_paw_print.png')" width="10%" />
+
+      <p style="font-size: small"><br /><b>MyPet</b></p>
+    </div>
+    <div class="Prijava">
+      <form>
+        <div class="form-group">
+          <label for="exampleInputEmail1" style="color: white">Email</label>
+          <input
+            type="email"
+            class="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+          />
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1" style="color: white"
+            >Lozinka</label
+          >
+          <input
+            type="password"
+            class="form-control"
+            id="exampleInputPassword1"
+          />
+        </div>
+        <button type="submit" class="button-Prijava">Prijava</button>
+      </form>
+    </div>
+  </body>
 </template>
+
+<style>
+body {
+  background: #ff344c;
+}
+
+.Zaglavlje {
+  background: white;
+  padding: 20px;
+  border-bottom-left-radius: 20px;
+  border-bottom-right-radius: 20px;
+  margin-bottom: 5%;
+}
+.button-Prijava {
+  background-color: black;
+  color: white;
+  margin-bottom: 10%;
+  margin-top: 5%;
+  border-radius: 20px;
+  padding: 10px;
+  border: none;
+  text-decoration: none;
+  width: 180px;
+  font-weight: 600;
+}
+.Prijava {
+  width: 300px;
+  margin: 0 auto;
+}
+.form-control {
+  margin-bottom: 5%;
+  border-radius: 20px;
+}
+.form-group {
+  text-align: left;
+}
+</style>
