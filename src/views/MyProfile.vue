cfa921ddbd38376f776909c9b8300a9386cf9354
Martina Rubil <92204071+mrubil@users.noreply.github.com>
2024-02-12 14:19:38 +0100

    Update MyProfile.vue

--- src/views/MyProfile.vue
+++ src/views/MyProfile.vue
@@ -3,6 +3,7 @@
     <div class="ZaglavljeProfila">
       <div class="Odjava">Odjava</div>
       <div class="SlikaProfila">Ovdje ide slika profila</div>
+      <div class="Ime">Ime ljubimca</div>
       <div class="Info">Informacije o ljubimcu</div>
     </div>
   </body>
@@ -14,11 +15,21 @@
 }
 .ZaglavljeProfila {
   background: white;
-  padding: 20px;
+  padding: 10px;
   border-bottom-left-radius: 20px;
   border-bottom-right-radius: 20px;
   margin-bottom: 5%;
 }
 .Odjava {
+  padding: 5px;
+}
+.SlikaProfila {
+  padding: 10px;
+}
+.Ime {
+  padding: 10px;
+}
+.Informacije {
+  padding: 10px;
 }
 </style>
