b2f2ec6885936fef0614ca682d2ca6f478566c2c
Martina Rubil <92204071+mrubil@users.noreply.github.com>
2024-02-17 14:01:26 +0100

    Navbar update

--- src/components/Navigacija.vue
+++ src/components/Navigacija.vue
@@ -2,17 +2,19 @@
   <nav class="navbar fixed-bottom navbar-light bg-light rounded-top-4">
     <a class="navbar-brand position-absolute" href="#"></a>
     <div class="mx-auto">
-      <router-link to="/Tasks"
+      <router-link to="/Tasks" class="nav-link"
         ><img :src="require('@/assets/Tasks.png')" width="5%"
       /></router-link>
 
-      <router-link to="/Kalendar"
+      <router-link to="/Kalendar" class="nav-link"
         ><img :src="require('@/assets/Kalendar.png')" width="5%"
       /></router-link>
-      <router-link to="/NewTask"
+
+      <router-link to="/NewTask" class="nav-link"
         ><img :src="require('@/assets/NewTask.png')" width="5%"
       /></router-link>
-      <router-link to="/MyProfile"
+
+      <router-link to="/MyProfile" class="nav-link"
         ><img :src="require('@/assets/MojProfil.png')" width="5%"
       /></router-link>
     </div>
