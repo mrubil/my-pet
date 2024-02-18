d7b357a3ef24a12c6ff9fedb10e412809d5495c9
Martina Rubil <92204071+mrubil@users.noreply.github.com>
2024-02-18 17:50:29 +0100

    Mape.vue update

--- src/components/Mape.vue
+++ src/components/Mape.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="row mx-auto" id="Mapa">
-    <div class="col-4 col-sm-4 col-lg-4" id="Zdravlje">Image</div>
+    <div class="col-4 col-sm-4 col-lg-4" id="Zdravlje">
+      <img :src="require('@/assets/Zdravlje.png')" width="100vw" />
+    </div>
 
-    <div class="col-4 col-sm-4 col-lg-4" id="Prehrana">Prehrana</div>
+    <div class="col-4 col-sm-4 col-lg-4" id="Prehrana">
+      <img :src="require('@/assets/Prehrana.jpg')" width="100vw" />
+    </div>
 
-    <div class="col-4 col-sm-4 col-lg-4" id="Prehrana">Aktivnost</div>
+    <div class="col-4 col-sm-4 col-lg-4" id="Prehrana">
+      <img :src="require('@/assets/Aktivnosti.png')" width="100vw" />
+    </div>
   </div>
 </template>
 
