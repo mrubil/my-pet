fe3ec00a8578fb92ccf73e8421b3da0014d350dc
Martina Rubil <92204071+mrubil@users.noreply.github.com>
2024-06-14 15:25:49 +0200

    Update UpcomingTask.vue

--- src/components/UpcomingTask.vue
+++ src/components/UpcomingTask.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="card mx-auto rounded-4" style="width: 90%" id="kartica">
     <div class="card-body">
-      <div class="row" style="font-size: 13px">
-        <div class="col-5 align-self-center">{{ task.naziv }}</div>
+      <div class="row justify-content-center" style="font-size: 13px">
+        <div class="col-4 align-self-center">{{ task.naziv }}</div>
         <div class="col-4 align-self-center" style="color: black">
           {{ task.datum }}
         </div>
-        <div class="col-3 align-self-center" style="color: black">
+        <div class="col-2 align-self-center px-0" style="color: black">
           {{ task.vrijeme }}
+        </div>
+        <div class="col-2 align-self-center px-0">
+          <button @click="UrediZadatak" class="gumb">Uredi</button>
         </div>
       </div>
     </div>
@@ -17,6 +20,14 @@
 <script>
 export default {
   props: ["task"],
+  methods: {
+    UrediZadatak() {
+      this.$router.push({
+        name: "EditTask",
+        params: { id: this.task.id },
+      });
+    },
+  },
 };
 </script>
 
@@ -24,5 +35,9 @@ export default {
 #kartica {
   font-weight: 600;
   margin-top: 20px;
+}
+.gumb {
+  background: none;
+  border: none;
 }
 </style>
